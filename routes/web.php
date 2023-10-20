a9deb7069bf095afa6a0fc86db096e45e024d249
Thor Malmby Jørgin <tjoer21@student.sdu.dk>
2023-10-20 22:57:04 +0200

    Back to redirecting to /cars from /

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hello World";
+    return redirect('/cars');
 });
 Route::get('/cars', [CarController::class, 'getAll']);
 Route::post('/cars', [CarController::class, 'store']);
