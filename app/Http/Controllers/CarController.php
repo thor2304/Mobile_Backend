971c6ab72e47b61092292b73265bec5f469a456c
Thor Jørgin <tjoer21@student.sdu.dk>
2023-12-07 15:22:16 +0100

    You got mail

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CarCollection;
 use App\Http\Resources\CarResource;
 use App\Models\Car;
+use Illuminate\Http\Request;
+use function Psy\debug;
 
 class CarController extends Controller
 {
@@ -22,20 +24,20 @@ class CarController extends Controller
         return new CarResource($car);
     }
 
-    public function store(): CarResource
+    public function store(Request $request): CarResource
     {
         $car = new Car();
-        $car->model = request()->only('model')['model'];
-        $car->manufacturer_id = request()->only('manufacturerId ')['manufacturerId'];
-        $car->engine_cc_size = request()->only('engineCCSize')['engineCCSize'];
-        $car->fuel_type_id = request()->only('fuelTypeId')['fuelTypeId'];
-        $car->price_per_day = request()->only('pricePerDay')['pricePerDay'];
-        $car->price_per_week = request()->only('pricePerWeek')['pricePerWeek'];
-        $car->doors = request()->only('doors')['doors'];
-        $car->description = request()->only('description')['description'];
-        $car->transmission_id = request()->only('transmissionId')['transmissionId'];
-        $car->wheel_size = request()->only('wheelSize')['wheelSize'];
-        $car->image_name = request()->only('imageName')['imageName'];
+        $car->model = $request->only('model')['model'];
+        $car->manufacturer_id = $request->only('manufacturerId')['manufacturerId'];
+        $car->engine_cc_size = $request->only('engineCCSize')['engineCCSize'];
+        $car->fuel_type_id = $request->only('fuelTypeId')['fuelTypeId'];
+        $car->price_per_day = $request->only('pricePerDay')['pricePerDay'];
+        $car->price_per_week = $request->only('pricePerWeek')['pricePerWeek'];
+        $car->doors = $request->only('doors')['doors'];
+        $car->description = $request->only('description')['description'];
+        $car->transmission_id = $request->only('transmissionId')['transmissionId'];
+        $car->wheel_size = $request->only('wheelSize')['wheelSize'];
+        $car->image_name = $request->only('imageName')['imageName'];
 
         $car->save();
 
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        "http://127.0.0.1:8000/cars",
+        "http://mobiledev.cryptobot.dk/cars",
+        "https://mobiledev.cryptobot.dk/cars",
     ];
 }
